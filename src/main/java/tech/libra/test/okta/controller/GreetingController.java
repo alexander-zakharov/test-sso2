1a2924e1af3568b382bc4899f206fc8e7ef7d0ff
Alexandr Zakharov <zalexs@ip-192-168-1-121.ec2.internal>
2018-12-31 18:32:46 -0500

    add token

--- src/main/java/tech/libra/test/okta/controller/GreetingController.java
+++ src/main/java/tech/libra/test/okta/controller/GreetingController.java
@@ -1,16 +1,24 @@
 package tech.libra.test.okta.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
 	@GetMapping("/")
 	public String home() {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("token", getAccessToken());
 		return "index";
 	}
 	
@@ -20,4 +28,19 @@ public class GreetingController {
         return "greeting";
     }
 
+    
+    private String getAccessToken() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        if (securityContext == null) return "no_token";
+        
+        Authentication authentication = securityContext.getAuthentication();
+        if (!(authentication instanceof OAuth2Authentication) && !(authentication.getDetails() instanceof OAuth2AuthenticationDetails)) return "no_token";
+
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)authentication.getDetails();
+        
+        System.out.println(details.getTokenValue());
+        
+        return details.getTokenValue();
+
+    }    
 }
