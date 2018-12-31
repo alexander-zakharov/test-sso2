5f11da1e5c02bd6bfe1254089d758dd8c393ef2b
Alexandr Zakharov <zalexs@ip-192-168-1-121.ec2.internal>
2018-12-31 18:18:24 -0500

    add controller

--- src/main/java/tech/libra/test/okta/controller/GreetingController.java
+++ src/main/java/tech/libra/test/okta/controller/GreetingController.java
@@ -2,12 +2,18 @@ package tech.libra.test.okta.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
